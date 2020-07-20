2c17cb0a5e2f94d6a47f47500994f522031557a8
luizinhovao <luiz.emof@gmail.com>
2020-07-20 18:25:25 -0300

    [Luiz] Implement Login View

--- src/IdentityServer/Controllers/Account/AccountController.cs
+++ src/IdentityServer/Controllers/Account/AccountController.cs
@@ -1,4 +1,5 @@
 using IdentityServer.Constants;
+using IdentityServer.Models.Account;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,12 @@ namespace IdentityServer.Controllers.Account
     public class AccountController : Controller
     {
         public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel loginModel)
         {
             return View();
         }
---
+++ src/IdentityServer/Models/Account/LoginModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServer.Models.Account
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool Remember { get; set; }
+    }
+}
