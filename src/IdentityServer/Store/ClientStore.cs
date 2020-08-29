276c937ed75d2fd37e4c26594ee701c377b20799
luizinhovao <luiz.emof@gmail.com>
2020-08-29 20:40:57 -0300

    [Luiz] Setting token lifetime

--- src/IdentityServer/Store/ClientStore.cs
+++ src/IdentityServer/Store/ClientStore.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using IdentityServer.Services;
 using IdentityServer4.Models;
 using IdentityServer4.Stores;
+using System;
 
 namespace IdentityServer.Store
 {
@@ -17,6 +18,7 @@ namespace IdentityServer.Store
         public async Task<Client> FindClientByIdAsync(string clientId)
         {
             var client = await ClientService.GetClientByClientIdAsync(clientId);
+            client.AccessTokenLifetime = (int)(TimeSpan.FromDays(1).TotalSeconds);
             return client;
         }
     }
